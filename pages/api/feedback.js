ce51ec7e4109a9090f1a4751940cfda6b525d9b5
gkgjswls <gkgjswls842@gmail.com>
2023-05-10 20:31:51 +0900

    리팩토링

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,5 +1,15 @@
 import fs from 'fs';
 import path from 'path';
+const buildFeedbackPath = () =>{
+
+  return path.join(process.cwd(),'data','feedback.json');
+}
+
+const extractFeedback = (filePath) =>{
+  const fileData = fs.readFileSync(filePath);
+  const data = JSON.parse(fileData)
+  return data;
+}
 
 const handler = (req, res) =>{
   if(req.method ==='POST'){
@@ -12,15 +22,16 @@ const handler = (req, res) =>{
       text: feedback,
     }
 
-    const filePath = path.join(process.cwd(),'data','feedback.json');
-    const fileData = fs.readFileSync(filePath);
-    const data = JSON.parse(fileData)
+    const filePath = buildFeedbackPath();
+    const data = extractFeedback(filePath)
     data.push(newFeedback)
     fs.writeFileSync(filePath,JSON.stringify(data))
 
     res.status(201).json({message: 'Success!' ,feedback: newFeedback})
   }else{
-    res.status(200).json({message: 'This  works!'})
+    const filePath = buildFeedbackPath();
+    const data = extractFeedback(filePath)
+    res.status(200).json({feedback: data})
 
   }
 
